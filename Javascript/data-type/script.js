9defe3750a68b5670503c9d29f89117d4ffc7f3f
shubhashisdark <shubhashisbaral@gmail.com>
2026-09-11 00:57:12 +0530

    addded primtive datatype

---
+++ Javascript/data-type/script.js
@@ -0,0 +1,33 @@
+
+//string types
+
+const name ="shubhashis";
+//number type
+const age = 44;
+
+console.log(typeof age)
+console.log(typeof name)
+//bigint type
+let bigN = 55555555555n;
+
+console.log(typeof bigN)
+
+//undefined type
+let a;
+console.log(a)
+//null type print object
+let v = null;
+
+console.log(typeof v)
+
+//symbol datatype
+
+const unque = Symbol("name");
+console.log(typeof unque)
+
+
+//boolean
+
+
+const ispass = true
+console.log(typeof ispass)
