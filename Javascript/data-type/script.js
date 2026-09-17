dade230068b8cbac8e553b7d83a43a4bcb2dd7c3
shubhashisdark <shubhashisbaral@gmail.com>
2026-09-17 10:22:07 +0530

    continue learn function

--- Javascript/data-type/script.js
+++ Javascript/data-type/script.js
@@ -31,3 +31,74 @@ console.log(typeof unque)
 
 const ispass = true
 console.log(typeof ispass)
+
+
+
+
+///reference datatype
+
+
+//Array set of memory 
+const students = ["raju","shytam","radhe","lulu"];
+
+console.log(students[3])
+
+//OBJECTS
+const obj = {
+    name:"silu",
+    age:88,
+    dob:"10/05/2003"
+};
+console.log(typeof obj)
+
+//function reference data type 
+
+//function is a reusable code
+
+function greet() {
+    console.log("hey how are you")
+};
+greet()
+
+
+//parametere argruments
+
+
+//dynamic function 
+
+
+function sum(num1,num2){
+    console.log(num1+num2);
+
+};
+sum(40,50)
+
+
+
+let user = {
+    user: "shubhashis",
+age: 78,
+college:"usbm"
+
+};
+
+console.log(user)
+
+
+//function is a reusable block of code called function 
+
+function division(num1,num2){
+    console.log(num1-num2)
+
+};
+division(50,45);
+
+//welcome
+
+
+function welcome(name){
+    console.log("hello "  + name)
+};
+
+welcome("shubhashis")
+welcome("daniel")
